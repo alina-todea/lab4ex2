13f9f8291ffe0e4d60cdaa5e54447e3fac312f69
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-18 23:10:24 +0300

    Update Program.cs
    removed class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using lab4ex2;
 
 namespace lab4ex1
 {
@@ -23,8 +24,8 @@ Creeati doua obiecte de tipul autoturism in functia “Main” apelati-le metode
              */
 
 
-            autoturism Ford = new autoturism("Ford", "CJ03 123", 1600);
-            autoturism Audi = new autoturism("Audi", "CJ01 456", 1800);
+            Autoturism Ford = new Autoturism("Ford", "CJ03 123", 1600);
+            Autoturism Audi = new Autoturism("Audi", "CJ01 456", 1800);
 
             Console.WriteLine($"Primul autoturism:\t{Ford.GetDescription(Ford.GetMarca(), Ford.GetNumarInmatriculare(), Ford.GetCapacitateCilindrica())}");
             Console.WriteLine($"Al doilea  autoturism:\t{Audi.GetDescription(Ford.GetMarca(), Audi.GetNumarInmatriculare(), Audi.GetCapacitateCilindrica())}");
@@ -33,69 +34,6 @@ Creeati doua obiecte de tipul autoturism in functia “Main” apelati-le metode
         }
 
 
-        
-
-
-        class autoturism
-        {
-
-            private string marca;
-            private string numarInmatriculare;
-            private int capacitateCilindrica;
-
-
-
-            public autoturism(string marca, string numarInmatriculare, int capacitateCilindrica)
-            {
-
-                this.marca = marca;
-                this.numarInmatriculare = numarInmatriculare;
-                this.capacitateCilindrica = capacitateCilindrica;
-            }
-
-            /// <summary>
-            /// returneaza marca
-            /// </summary>
-            /// <returns></returns>
-            public string GetMarca()
-            {
-                return marca;
-            }
-
-            /// <summary>
-            /// returneaza nr de inmatriculare
-            /// </summary>
-            /// <returns></returns>
-            public string GetNumarInmatriculare()
-            {
-                return numarInmatriculare;
-            }
-
-            /// <summary>
-            /// returneaza capacitatea cilindrica
-            /// </summary>
-            /// <returns></returns>
-            public int GetCapacitateCilindrica()
-            {
-                return capacitateCilindrica;
-            }
-
-            /// <summary>
-            /// returneaza descrierea incluzand marca, nr de inmatriculare, capacitatea cilindrica
-            /// </summary>
-            /// <param name="marca"></param>
-            /// <param name="numarInmatriculare"></param>
-            /// <param name="capacitateCilindrica"></param>
-            /// <returns></returns>
-            public string GetDescription(string marca, string numarInmatriculare, int capacitateCilindrica)
-            { 
-                ;
-
-                return GetMarca() + ", " + GetNumarInmatriculare() + ", " + GetCapacitateCilindrica();
-            }
-
-
-
-        }
     }
 }
+
